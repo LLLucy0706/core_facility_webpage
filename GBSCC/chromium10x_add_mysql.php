4bb43cbba1acf34f3092ff0b94ab0201b747f524
LLLucy0706 <84826516+LLLucy0706@users.noreply.github.com>
2021-07-21 16:09:46 +0800

    Update chromium10x_add_mysql.php

--- GBSCC/chromium10x_add_mysql.php
+++ GBSCC/chromium10x_add_mysql.php
@@ -123,9 +123,8 @@ if (!$res) {
         $mail->SMTPSecure = "ssl";
         $mail->Host       = "smtp.gmail.com";
         $mail->Port       = 465;
-        $mail->Username   = "fhs.genomics.core@gmail.com";
+        $mail->Username   = "******";
         $mail->Password   = "******";
-        $mail->SetFrom('fhs.genomics.core@gmail.com', 'fhs.genomics.core');
         $mail->Subject    = $Subject;
         $mail->MsgHTML($main_mesg);
 
@@ -138,9 +137,8 @@ if (!$res) {
         $mail->SMTPSecure = "tls";
         $mail->Host = "smtp.office365.com";
         $mail->Port = 587;
-        $mail->Username = "fhs.genomics.core@outlook.com";
+        $mail->Username = "******";
         $mail->Password = "******";
-        $mail->SetFrom('fhs.genomics.core@outlook.com', 'fhs.genomics.core');
         $mail->Subject = $Subject;
         $mail->MsgHTML($main_mesg);
         */
